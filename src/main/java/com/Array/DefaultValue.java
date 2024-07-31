c26f905fea68c877f627a55da924ec65a289d6f9
mgaur101@gmail.com <mgaur101@gmail.com>
2024-07-31 21:21:01 +0530

    Adding program to show default values assigned to array elements.

---
+++ src/main/java/com/Array/DefaultValue.java
@@ -0,0 +1,10 @@
+package com.Array;
+
+public class DefaultValue {
+    public static void main(String[] args) {
+        int marks[] = new int[4];
+        String name[] = new String[4];
+        System.out.println(name[0]);// Default value of String will be assigned means null
+        System.out.println(marks[0]); //Default value of int will be assigned means 0
+    }
+}
