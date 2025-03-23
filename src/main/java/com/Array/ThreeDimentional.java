9b3d5c86c6a3d502ad1051488806afd8a02948e6
mgaur101@gmail.com <mgaur101@gmail.com>
2025-03-24 01:29:58 +0530

    Multidimentional array program

--- src/main/java/com/Array/ThreeDimentional.java
+++ src/main/java/com/Array/ThreeDimentional.java
@@ -8,6 +8,15 @@ public class ThreeDimentional {
     //compile error - Java does not allow complex initializations like multi-dimensional jagged arrays directly.
     //m[0] = new int[3][];
 
+    //Constructor
+    ThreeDimentional(){
+        m[0] = new int[3][];
+        m[0][0] =  new int[1];
+        m[0][1] = new int[2];
+        m[0][2] = new int[3];
+        m[1] = new int[2][2];
+    }
+
     /**
      * Instance arrays should be initialized inside a constructor or method.
      * Jagged arrays require step-by-step initialization, which can't be done in the class field directly.
@@ -16,7 +25,31 @@ public class ThreeDimentional {
 
     public static void main(String [] args){
         ThreeDimentional obj = new ThreeDimentional(); // Object reference created
-        obj.m[0] = new int[3][];
+        System.out.println(obj.m.length);
+        System.out.println(obj.m[0].length);
+        System.out.println(obj.m[0][0].length);
+        System.out.println(obj.m[0][1].length);
+        System.out.println(obj.m[0][2].length);
+
+        //Assign values to 3D arrays
+        int counter = 1;
+        for(int i = 0; i < obj.m.length; i++){
+            for(int j = 0; j < obj.m[i].length; j++){
+                for(int k = 0; k < obj.m[i][j].length;k++){
+                    obj.m[i][j][k] = counter++;
+                }
+            }
+        }
+
+        //Reading values from 3D array
+        System.out.println("-------------------------- Printing 3d array values. ---------------------");
+        for(int i = 0; i < obj.m.length; i++){
+            for(int j = 0; j < obj.m[i].length; j++){
+                for(int k = 0; k < obj.m[i][j].length;k++){
+                    System.out.println("Value at ["+i+"]["+j+"]["+k+"] = "+ obj.m[i][j][k]);
+                }
+            }
+        }
 
     }
 
