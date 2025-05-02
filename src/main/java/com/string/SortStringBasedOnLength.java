52a75e59e90c5ea3959043dbbc41066e382a571c
mgaur101@gmail.com <mgaur101@gmail.com>
2025-05-03 01:57:42 +0530

    Added String word sort methods

--- src/main/java/com/string/SortStringBasedOnLength.java
+++ src/main/java/com/string/SortStringBasedOnLength.java
@@ -9,5 +9,22 @@ public class SortStringBasedOnLength {
         String [] arr = s.split(" ");
         Arrays.sort(arr, Comparator.comparingInt(String::length));
         System.out.println("Sorted string elements based on length: \n"+Arrays.toString(arr));
+        System.out.println(sortWordsInAscendingLength(s));
+        System.out.println(sortWordsInDescendingLength(s));
+    }
+
+    private static String sortWordsInDescendingLength(String s){
+        String [] s1 = s.split(" ");
+        Arrays.sort(s1,(a,b) -> Integer.compare(b.length(),a.length()));
+        return String.join(" ",s1);
+    }
+
+    private static String sortWordsInAscendingLength(String s){
+        String [] s1 = s.split(" ");
+        //Arrays.sort(s1,Comparator.comparingInt(String::length));
+
+        //or
+        Arrays.sort(s1,(a,b) -> Integer.compare(a.length(), b.length()));
+        return String.join(" ",s1);
     }
 }
