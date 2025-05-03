0a8f100e15199478655a702ff1e0b421aa065d04
mgaur101@gmail.com <mgaur101@gmail.com>
2025-05-03 19:26:11 +0530

    Added method for String swap without temp variable

--- src/main/java/com/string/SwapStringWithoutTemp.java
+++ src/main/java/com/string/SwapStringWithoutTemp.java
@@ -9,6 +9,20 @@ public class SwapStringWithoutTemp {
         s1 =  s1+s2;
         s2 = s1.substring(0,s1.length() - s2.length());
         s1 = s1.substring(s2.length());
-        System.out.printf("\nModified:\ns1: %s\ns2: %s",s1,s2);
+        System.out.printf("\nModified:\ns1: %s\ns2: %s \n",s1,s2);
+
+        String [] listswappedStrings = swapStrings(s1,s2);
+        System.out.println(listswappedStrings[0]);
+        System.out.println(listswappedStrings[1]);
+
+    }
+
+
+    private static String[] swapStrings(String a, String b){
+        a = a + b;
+        b = a.substring(0,a.length() - b.length());
+        a = a.substring(b.length());
+
+        return new String[] {a,b};
     }
 }
